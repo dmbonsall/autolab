df1f2706d1540b6a76ced51e698a8657a515a141
David Bonsall <david.bonsall@hotmail.com>
2021-12-23 20:15:07 -0600

    Add enable/start to the install script

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ chmod 755 /usr/local/bin/autolab-start
 # ===== Copy the systemd unit file =====
 cp ./autolab.service /etc/systemd/system/
 chmod 664 /etc/systemd/system/autolab.service
+
+systemctl daemon-reload
+systemctl enable autolab.service
+systemctl start autolab.service
