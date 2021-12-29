917cfd6a4ebde2078ae1344a4fd6c5a5eb462617
David Bonsall <david.bonsall@hotmail.com>
2021-12-28 20:14:28 -0600

    Update UI to use the new configuration backup endpoint.

--- static/js/create-vm.js
+++ static/js/create-vm.js
@@ -8,7 +8,7 @@ $(function () {
 
         // ===== Set the status text, then disable the create button =====
         console.log(data)
-        $("#out_form").html("Creating vm...")
+        $("#createVmOut").html("Creating vm...")
         $("#createVmButton").attr("disabled", true)
 
         // ===== Place the REST request =====
@@ -20,13 +20,34 @@ $(function () {
             type: "POST",
             success: function (response) {
                 console.log(response)
-                $("#out_form").html("Success! Ip Addresses:\n" + response.ip_addrs.toString())
+                $("#createVmOut").html("Success! Ip Addresses:\n" + response.ip_addrs.toString())
                 $("#createVmButton").removeAttr("disabled")
             },
             error: function (response) {
                 console.log(response)
-                $("#out_form").html("Failure:\n" + response)
+                $("#createVmOut").html("Failure:\n" + response)
                 $("#createVmButton").removeAttr("disabled")
+            }
+        });
+    });
+    $("#configBackupButton").click(function () {
+        // ===== Set the status text, then disable the backup button =====
+        $("#configBackupOut").html("Backing up configurations.....")
+        $("#configBackupButton").attr("disabled", true)
+
+        // ===== Place the REST request =====
+        $.ajax({
+            url: "/api/config-backup",
+            type: "POST",
+            success: function (response) {
+                console.log(response)
+                $("#configBackupOut").html("Success!")
+                $("#configBackupButton").removeAttr("disabled")
+            },
+            error: function (response) {
+                console.log(response)
+                $("#configBackupOut").html("Failure:\n" + response)
+                $("#configBackupButton").removeAttr("disabled")
             }
         });
     });
