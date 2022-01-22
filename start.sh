7d5d1f66b83882368d7c953512c154b570ba1041
David Bonsall <david.bonsall@hotmail.com>
2022-01-22 15:47:01 -0600

    Added SSL arguments

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd $HOME
 source $HOME/venv/bin/activate
-uvicorn --host 0.0.0.0 autolab.api:app --log-config $HOME/logging.json
+uvicorn --host 0.0.0.0 \
+    --log-config $HOME/logging.json \
+    --ssl-keyfile $HOME/autolab.key \
+    --ssl-certfile $HOME/autolab.crt \
+    autolab.api:app
